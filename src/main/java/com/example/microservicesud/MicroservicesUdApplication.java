589779e7b99790e57065aa71053e209cfb907014
Bart Lintsen <bart.lintsen@hva.nl>
2023-05-10 09:44:55 +0200

    git initial commit

---
+++ src/main/java/com/example/microservicesud/MicroservicesUdApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicesud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesUdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesUdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/microservicesud/MicroservicesUdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicesud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesUdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
